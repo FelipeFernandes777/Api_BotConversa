fb34f3aaf5f00f624148491b754f781cdd21322f
FelipeFernandes777 <felipe524.fernandes@gmail.com>
2025-04-02 09:22:50 -0300

    First commit

---
+++ src/main/java/br/com/educaminas/api_bot_conversa/ApiBotConversaApplication.java
@@ -0,0 +1,13 @@
+package br.com.educaminas.api_bot_conversa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBotConversaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBotConversaApplication.class, args);
+	}
+
+}
